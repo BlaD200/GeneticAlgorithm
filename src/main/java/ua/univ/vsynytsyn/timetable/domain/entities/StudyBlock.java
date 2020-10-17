df5a448e369c088b55a67ffc910125fc0f044a3d
BlaD200 <blad600@gmail.com>
2020-10-18 00:00:31 +0300

    Added entity StudyBlock (for no reason)

---
+++ src/main/java/ua/univ/vsynytsyn/timetable/domain/entities/StudyBlock.java
@@ -0,0 +1,23 @@
+package ua.univ.vsynytsyn.timetable.domain.entities;
+
+import lombok.Data;
+import ua.univ.vsynytsyn.timetable.domain.data.LessonType;
+
+import javax.persistence.*;
+
+@SuppressWarnings("JpaDataSourceORMInspection")
+@Data
+@Entity
+@Table(name = "study_block")
+public class StudyBlock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String studyBlockID;
+
+    private Long lessonID;
+    private Long lectorID;
+    private Long groupID;
+    private int studentsCount;
+    private LessonType lessonType;
+}
