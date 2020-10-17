ecca78aeabf7a9e7f1785175b37426975494d3d8
Iron Tony <lifegamecom1@gmail.com>
2020-10-17 23:13:58 +0300

    Saving Loaded Entities to DB

--- src/main/java/ua/univ/vsynytsyn/timetable/controllers/CsvLoaderController.java
+++ src/main/java/ua/univ/vsynytsyn/timetable/controllers/CsvLoaderController.java
@@ -1,5 +1,6 @@
 package ua.univ.vsynytsyn.timetable.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import ua.univ.vsynytsyn.timetable.domain.entities.*;
 import ua.univ.vsynytsyn.timetable.exceptions.CsvDeserializableException;
+import ua.univ.vsynytsyn.timetable.service.LoadService;
 import ua.univ.vsynytsyn.timetable.utils.CsvUtils;
 
 import java.io.IOException;
@@ -17,34 +19,51 @@ import java.util.List;
 @RequestMapping("/load/csv")
 public class CsvLoaderController {
 
+    private final LoadService loadService;
+
+    @Autowired
+    public CsvLoaderController(LoadService loadService) {
+        this.loadService = loadService;
+    }
+
     @PostMapping(value = "/auditoriums")
     public List<Auditorium> auditoriums(@RequestParam("file") MultipartFile file)
             throws IOException, IllegalAccessException, CsvDeserializableException, InvocationTargetException {
-        return load(file, Auditorium.class);
+        List<Auditorium> auditoriums = load(file, Auditorium.class);
+        loadService.saveAuditoriums(auditoriums);
+        return auditoriums;
     }
 
     @PostMapping(value = "/groups")
     public List<Group> groups(@RequestParam("file") MultipartFile file)
             throws IOException, IllegalAccessException, CsvDeserializableException, InvocationTargetException {
-        return load(file, Group.class);
+        List<Group> groups = load(file, Group.class);
+        loadService.saveGroups(groups);
+        return groups;
     }
 
     @PostMapping(value = "/lectors")
     public List<Lector> lectors(@RequestParam("file") MultipartFile file)
             throws IOException, IllegalAccessException, CsvDeserializableException, InvocationTargetException {
-        return load(file, Lector.class);
+        List<Lector> lectors = load(file, Lector.class);
+        loadService.saveLectors(lectors);
+        return lectors;
     }
 
     @PostMapping(value = "/lessons")
     public List<Lesson> lessons(@RequestParam("file") MultipartFile file)
             throws IOException, IllegalAccessException, CsvDeserializableException, InvocationTargetException {
-        return load(file, Lesson.class);
+        List<Lesson> lessons = load(file, Lesson.class);
+        loadService.saveLessons(lessons);
+        return lessons;
     }
 
     @PostMapping(value = "/timeslots")
     public List<TimeSlot> timeslots(@RequestParam("file") MultipartFile file)
             throws IOException, IllegalAccessException, CsvDeserializableException, InvocationTargetException {
-        return load(file, TimeSlot.class);
+        List<TimeSlot> timeSlots = load(file, TimeSlot.class);
+        loadService.saveTimeSlots(timeSlots);
+        return timeSlots;
     }
 
     public <T> List<T> load(MultipartFile file, Class<T> clazz)
--- src/main/java/ua/univ/vsynytsyn/timetable/controllers/JsonLoaderController.java
+++ src/main/java/ua/univ/vsynytsyn/timetable/controllers/JsonLoaderController.java
@@ -1,11 +1,13 @@
 package ua.univ.vsynytsyn.timetable.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import ua.univ.vsynytsyn.timetable.domain.entities.*;
+import ua.univ.vsynytsyn.timetable.service.LoadService;
 import ua.univ.vsynytsyn.timetable.utils.JsonUtils;
 
 import java.io.IOException;
@@ -15,29 +17,46 @@ import java.util.List;
 @RequestMapping("/load/json")
 public class JsonLoaderController {
 
+    private final LoadService loadService;
+
+    @Autowired
+    public JsonLoaderController(LoadService loadService) {
+        this.loadService = loadService;
+    }
+
     @PostMapping(value = "/auditoriums")
     public List<Auditorium> auditoriums(@RequestParam("file") MultipartFile file) throws IOException, ClassNotFoundException {
-        return load(file, Auditorium.class);
+        List<Auditorium> auditoriums = load(file, Auditorium.class);
+        loadService.saveAuditoriums(auditoriums);
+        return auditoriums;
     }
 
     @PostMapping(value = "/groups")
     public List<Group> groups(@RequestParam("file") MultipartFile file) throws IOException, ClassNotFoundException {
-        return load(file, Group.class);
+        List<Group> groups = load(file, Group.class);
+        loadService.saveGroups(groups);
+        return groups;
     }
 
     @PostMapping(value = "/lectors")
     public List<Lector> lectors(@RequestParam("file") MultipartFile file) throws IOException, ClassNotFoundException {
-        return load(file, Lector.class);
+        List<Lector> lectors = load(file, Lector.class);
+        loadService.saveLectors(lectors);
+        return lectors;
     }
 
     @PostMapping(value = "/lessons")
     public List<Lesson> lessons(@RequestParam("file") MultipartFile file) throws IOException, ClassNotFoundException {
-        return load(file, Lesson.class);
+        List<Lesson> lessons = load(file, Lesson.class);
+        loadService.saveLessons(lessons);
+        return lessons;
     }
 
     @PostMapping(value = "/timeslots")
     public List<TimeSlot> timeslots(@RequestParam("file") MultipartFile file) throws IOException, ClassNotFoundException {
-        return load(file, TimeSlot.class);
+        List<TimeSlot> timeSlots = load(file, TimeSlot.class);
+        loadService.saveTimeSlots(timeSlots);
+        return timeSlots;
     }
 
     public <T> List<T> load(MultipartFile file, Class<T> clazz) throws IOException, ClassNotFoundException {
