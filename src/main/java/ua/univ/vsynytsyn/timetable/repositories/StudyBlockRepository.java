c1e8fe480dbfe7bfd27c1a9ba3df63f7ed97a20a
BlaD200 <blad600@gmail.com>
2020-10-18 10:44:21 +0300

    Genetic algorithm (Population class) done.

---
+++ src/main/java/ua/univ/vsynytsyn/timetable/repositories/StudyBlockRepository.java
@@ -0,0 +1,9 @@
+package ua.univ.vsynytsyn.timetable.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ua.univ.vsynytsyn.timetable.domain.data.StudyBlock;
+
+@Repository
+public interface StudyBlockRepository extends JpaRepository<StudyBlock, Long> {
+}
